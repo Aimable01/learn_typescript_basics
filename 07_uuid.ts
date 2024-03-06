f24e2db2ee18ba013170c285bef80a09becd71dc
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-06 20:48:24 +0200

    UUID generator

---
+++ 07_uuid.ts
@@ -0,0 +1,7 @@
+import { v4 as uuidv4 } from "uuid";
+
+const uuid_1: string = uuidv4();
+const uuid_2: string = uuidv4();
+
+console.log(`The first id: ${uuid_1}`);
+console.log(`The first id: ${uuid_2}`);
