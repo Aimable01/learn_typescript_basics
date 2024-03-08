7330ea7b2f00c5a032187fc6ae4ec7d04a7bda45
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-08 16:12:54 +0200

    add the pets schema

--- Drizzle/schema.ts
+++ Drizzle/schema.ts
@@ -1,8 +1,16 @@
 import { sql } from "drizzle-orm";
-import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const todos = pgTable("todos", {
-  id: uuid("id").default(sql`gen_random_uuid()`),
+export const owner = pgTable("owner", {
+  id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
   email: varchar("email").unique().notNull(),
+});
+
+export const pets = pgTable("pets", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 20 }).notNull(),
+  ownerId: integer("ownerId")
+    .references(() => owner.id)
+    .notNull(),
 });
