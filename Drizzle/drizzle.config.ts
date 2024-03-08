e7b42ff8055b2df221a49cbbb01eb3bbe2c52c51
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-08 07:03:50 +0200

    configure drizzle

--- Drizzle/drizzle.config.ts
+++ Drizzle/drizzle.config.ts
@@ -1,8 +1,8 @@
 import type { Config } from "drizzle-kit";
 
 export default {
-  schema: "./schema",
-  out: "./drizzle",
+  schema: "./Drizzle/schema.ts",
+  out: "./Drizzle/drizzle",
   driver: "pg",
   dbCredentials: {
     connectionString: "postgresql://postgres:2006@localhost:5432/drizzle",
