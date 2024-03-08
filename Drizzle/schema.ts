a164c7c8f84515f465445217308cfe0a3a20fb25
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-08 07:04:56 +0200

    the user schema

--- Drizzle/schema.ts
+++ Drizzle/schema.ts
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 
-export const user = pgTable("user", {
+export const todos = pgTable("todos", {
   id: uuid("id").default(sql`gen_random_uuid()`),
   name: varchar("name").notNull(),
   email: varchar("email").unique().notNull(),
