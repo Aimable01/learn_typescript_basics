bcbdd8eda9d65f3fd97da65765d922bb3a5832be
Aimable01 <aimable.kwizera14@gmail.com>
2024-03-12 15:44:17 +0200

    the main server handler

--- socket.io/index.js
+++ socket.io/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "node:http";
 import { join, dirname } from "node:path";
 import { fileURLToPath } from "node:url";
+import { Server, Socket } from "socket.io";
 
 const app = express();
 const server = createServer(app);
@@ -10,6 +11,15 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket");
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
 });
 
 server.listen(3000, () => console.log("Server is running on port 3000."));
